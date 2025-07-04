3f06f0f5964a3639a35bc878a6e153b0b6468bc8
faragzz <a.khaled46462@gmail.com>
2025-07-04 04:30:05 +0300

    feat: created Cart class

---
+++ src/Cart.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Cart {
+    List<CartItem> items = new ArrayList<>();
+
+    public void add(Product product, int quantity) {
+        if (quantity > product.getQuantity()) {
+            System.out.println("Not enough stock for " + product.getName() + " it can only get " + product.getQuantity());
+            return;
+        }
+        items.add(new CartItem(product, quantity));
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    public double getSubTotal() {
+        return items.stream().mapToDouble(CartItem::getTotalPrice).sum();
+    }
+
+    public double getShippingFee() {
+        double total = 0;
+        for (CartItem item : items) {
+            if (item.product instanceof Shippable) total += 10; // we can change it as we want
+        }
+        return total;
+    }
+
+    public List<Shippable> getShippableItems() {
+        List<Shippable> shippableList = new ArrayList<>();
+        for (CartItem item : items) {
+            if (item.product instanceof Shippable) {
+                for (int i = 0; i < item.quantity; i++) shippableList.add((Shippable) item.product);
+            }
+        }
+        return shippableList;
+    }
+}
